32a80f26d2f139749a513a353be3dc11b31c5575
John Moore <Guitarbum722@users.noreply.github.com>
2017-07-19 20:21:26 -0700

    add benchmarks for custom Split (#25)

--- align/align_test.go
+++ align/align_test.go
@@ -16,12 +16,38 @@ Karleigh,Destiny,Dean,nunc.In@lorem.edu,Stockholms län,Märsta,9038,Shaine Reil
 Alisa,Walker,Armand,Sed@Nuncmauriselit.com,Himachal Pradesh,Shimla,MZ0 4QS,Olivia Velez ,Alisa,Walker,Armand,Sed@Nuncmauriselit.com,Himachal Pradesh,Shimla,MZ0 4QS,Olivia Velez ,Alisa,Walker,Armand,Sed@Nuncmauriselit.com,Himachal Pradesh,Shimla,MZ0 4QS,Olivia Velez         
 `
 
-	sw := NewAligner(strings.NewReader(input), os.Stdout, ',')
+	sw := NewAligner(strings.NewReader(input), os.Stdout, ',', TextQualifier{On: false})
 
 	b.ReportAllocs()
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
 		_ = sw.ColumnCounts()
+	}
+}
+
+func BenchmarkSplitWithQual(b *testing.B) {
+	input := "First,\"Middle, name\",Last,Email,Region,City,Zip,Full_Name"
+
+	sw := NewAligner(strings.NewReader(input), os.Stdout, ',', TextQualifier{On: true, Qualifier: '"'})
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = sw.SplitWithQual(input, ",", "\"")
+	}
+}
+
+func BenchmarkSplitWithQualNoQual(b *testing.B) {
+	input := "First,Middle,Last,Email,Region,City,Zip,Full_Name"
+
+	sw := NewAligner(strings.NewReader(input), os.Stdout, ',', TextQualifier{On: false})
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = sw.SplitWithQual(input, ",", "\"")
 	}
 }
