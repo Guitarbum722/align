49895a46340ecf5b9ef1a7b4e6c4675074c8e06b
John Moore <johnkenneth.moore@gmail.com>
2019-11-17 09:10:00 -0700

    fix install command

--- build.sh
+++ build.sh
@@ -22,22 +22,22 @@ case "$1" in
     "freebsd") 
         echo "Building binary for FreeBSD..."
         cd ${CMD_DIR}
-        GOOS=freebsd GOARCH=amd64 ${BUILD_CMD} bin/align-freebsd
+        GOOS=freebsd GOARCH=amd64 ${BUILD_CMD} ../../bin/align-freebsd
         ;;
     "darwin") 
         echo "Building binary for Darwin..."
         cd ${CMD_DIR}
-        GOOS=darwin GOARCH=amd64 ${BUILD_CMD} bin/align-darwin
+        GOOS=darwin GOARCH=amd64 ${BUILD_CMD} ../../bin/align-darwin
         ;;
     "linux") 
         echo "Building binary for Linux..."
         cd ${CMD_DIR}
-        GOOS=linux GOARCH=amd64 ${BUILD_CMD} bin/align-linux
+        GOOS=linux GOARCH=amd64 ${BUILD_CMD} ../../bin/align-linux
         ;;
     "windows") 
         echo "Building binary for Windows..."
         cd ${CMD_DIR}
-        GOOS=windows GOARCH=amd64 ${BUILD_CMD} bin/align-windows.exe
+        GOOS=windows GOARCH=amd64 ${BUILD_CMD} ../../bin/align-windows.exe
         ;;
 esac
 
